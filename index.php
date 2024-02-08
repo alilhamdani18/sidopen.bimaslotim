71e13836a2f93b798c4a6bbfd2b069a75310d982
alilhamdani18 <hamdanialil18@gmail.com>
2024-02-08 19:49:17 +0800

    first commit

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require 'backend/cek-login.php';
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
   <meta name="description" content="" />
   <meta name="author" content="" />
-  <title>Sistem Informasi Manajemen Buku Nikah</title>
+  <title>Sistem Informasi Dokumen Pencatatan Nikah</title>
   <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
   <link href="assets/css/styles.css" rel="stylesheet" />
   <link rel="stylesheet" href="assets/css/style.css">
@@ -26,12 +26,12 @@ require 'backend/cek-login.php';
 <body class="sb-nav-fixed">
   <nav class="sb-topnav navbar navbar-expand navbar-dark bg-success">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3 fw-bold text-warning fs-3" href="index.php">SIMBAH</a>
+    <a class="navbar-brand ps-3 fw-bold text-warning fs-3" href="index.php">SIDOPEN</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
     <div class="col-lg-5 ms-3 my-2">
-      <h5 class="fw-semibold text-white title-nav d-lg-block d-none">Sistem Informasi Manajemen Buku Nikah</h5>
+      <h5 class="fw-semibold text-white title-nav d-lg-block d-none">Sistem Informasi Dokumen Pencatatan Nikah</h5>
     </div>
     <!-- <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <div class="input-group">
