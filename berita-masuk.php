e9467c87a8894f16f89a5b87e49684ddae47966f
alilhamdani18 <hamdanialil18@gmail.com>
2024-02-11 12:29:47 +0800

    add feature download berita acara

--- berita-masuk.php
+++ berita-masuk.php
@@ -57,7 +57,7 @@ require 'backend/cek-login.php';
                     $pihak_pertama = $data['pihak_pertama'];
                     $pihak_kedua = $data['pihak_kedua'];
                     $ekstensi = $data['ekstensi'];
-                    // $berkas = $data['berkas'];
+                    $berkas = $data['berkas'];
 
 
                   ?>
@@ -72,64 +72,24 @@ require 'backend/cek-login.php';
                       <td><?= $ekstensi; ?></td>
                       <!-- <td><?= $berkas; ?></td> -->
                       <td>
-                        <button class="btn btn-success action" data-bs-toggle="modal" data-bs-target="#"><i class="fa-solid fa-download"></i></button>
-                        <button class="btn btn-primary action my-1" data-bs-toggle="modal" data-bs-target="#editData<?= $id_masuk; ?>"><i class="fa-solid fa-pen-to-square"></i></button>
-                        <button class="btn btn-danger action" data-bs-toggle="modal" data-bs-target="#hapusData<?= $id_masuk; ?>"><i class="fa-solid fa-trash-can"></i></button>
+                        <button class="btn btn-success action" data-bs-toggle="modal" data-bs-target="#readData<?= $id_berita; ?>"><i class="fa-solid fa-download"></i></button>
+                        <button class="btn btn-primary action my-1" data-bs-toggle="modal" data-bs-target="#editData<?= $id_berita; ?>"><i class="fa-solid fa-pen-to-square"></i></button>
+                        <button class="btn btn-danger action" data-bs-toggle="modal" data-bs-target="#hapusData<?= $id_berita; ?>"><i class="fa-solid fa-trash-can"></i></button>
                       </td>
                     </tr>
-                    <!-- readData -->
-                    <div class="modal fade" id="readData<?= $id_masuk; ?>">
-                      <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <!-- Download Data -->
+                    <div class="modal fade" id="readData<?= $id_berita; ?>">
+                      <div class="modal-dialog modal-dialog-centered modal-md">
                         <div class="modal-content p-3">
                           <div class="modal-header py-2">
-                            <h4 class="modal-title fw-semibold">Details Data Masuk</h4>
+                            <h4 class="modal-title fw-bold">Download Berita Acara</h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
                           <div class="modal-body">
-                            <form action="" id="formRead">
-                              <div class="inputfield">
-                                <div>
-                                  <label for="tanggal">Tanggal</label>
-                                  <input type="date" name="tanggal" value="<?= $tanggal; ?>" disabled id="tanggal" />
-                                </div>
-                                <div>
-                                  <label for="uraian">Uraian</label>
-                                  <textarea name="uraian" disabled id="uraian" cols="30" rows="3"><?php echo $uraian; ?></textarea>
-                                </div>
-                                <div>
-                                  <label for="jmlMasuk">Jumlah Masuk</label>
-                                  <input type="number" name="jmlMasuk" value="<?= $banyak_masuk ?>" disabled id="jmlMasuk" />
-                                </div>
-                                <div class="row">
-                                  <div class="col-lg-6">
-                                    <label for="model">Model</label>
-                                    <select class="half" name="model" disabled id="model">
-                                      <option value="<?= $model; ?>"><?= $model; ?></option>
-                                    </select>
-                                  </div>
-                                  <div class="col-lg-6">
-                                    <label for="nomorSeri">Seri / Nomor</label>
-                                    <input class="half" type="text" name="nomorSeri" value="<?= $nomor_seri ?>" disabled id="nomorSeri" />
-                                  </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col-lg-6">
-                                    <div>
-                                      <label for="satuan">Satuan</label>
-                                      <input type="text" name="satuan" value="<?= $satuan; ?>" disabled id="satuan" />
-                                    </div>
-                                  </div>
-                                  <div class="col-lg-6">
-                                    <div>
-                                      <label for="nomorBukti">Nomor Bukti</label>
-                                      <input type="text" name="nomorBukti" value="<?= $nomor_bukti; ?>" disabled id="nomorBukti" />
-                                    </div>
-                                  </div>
-                                </div>
-                                <div>
-                                  <label for="ket">Keterangan</label>
-                                  <input type="text" name="ket" value="<?= $keterangan; ?>" disabled id="ket" />
-                                </div>
+                            <form action="" method="post">
+                              <h6>Download Berita Acara ini ?</h6>
+                              <div class="d-flex justify-content-center mt-5">
+                                <a class="btn btn-success" href="download-file.php?url=<?php echo $data['berkas']; ?>">Download</a>
                               </div>
                             </form>
                           </div>
@@ -138,68 +98,32 @@ require 'backend/cek-login.php';
                     </div>
 
                     <!-- Edit Data  -->
-                    <div class="modal fade" id="editData<?= $id_masuk; ?>">
+                    <div class="modal fade" id="editData<?= $id_berita; ?>">
                       <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content p-3">
                           <div class="modal-header py-2">
-                            <h4 class="modal-title fw-bold">Tambah Data Masuk</h4>
+                            <h4 class="modal-title fw-bold">Edit Data Masuk</h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
                           <div class="modal-body">
-                            <form action="" method="post">
+                            <form action="" method="POST" id="formBerita" enctype="multipart/form-data">
                               <div class="inputfield">
                                 <div>
-                                  <label for="tanggal">Tanggal</label>
-                                  <input type="date" name="tanggal" value="<?= $tanggal; ?>" id="tanggal" />
+                                  <label for="nomor">Nomor Surat</label>
+                                  <input type="text" name="nomor" value="<?= $nomor ?>" id="nomor" />
                                 </div>
                                 <div>
-                                  <label for="uraian">Uraian</label>
-                                  <textarea name="uraian" id="uraian" cols="30" rows="3"><?php echo $uraian; ?></textarea>
+                                  <label for="pihak_pertama">Pihak Pertama</label>
+                                  <input type="text" name="pihak_pertama" value="<?= $pihak_pertama ?>" id="pihak_pertama" />
                                 </div>
                                 <div>
-                                  <label for="jmlMasuk">Jumlah Masuk</label>
-                                  <input type="number" name="banyak_masuk" value="<?= $banyak_masuk; ?>" id="jmlMasuk" />
-                                </div>
-                                <div class="row">
-                                  <div class="col-lg-6">
-                                    <label for="model">Model</label>
-                                    <select class="half" name="model" id="model">
-                                      <option value="<?= $model; ?>"><?= $model; ?></option>
-                                      <option value="N">N</option>
-                                      <option value="NA">NA</option>
-                                      <option value="NB">NB</option>
-                                      <option value="R">R</option>
-                                      <option value="DN">DN</option>
-                                      <option value="RA">RA</option>
-                                    </select>
-                                  </div>
-                                  <div class="col-lg-6">
-                                    <label for="nomorSeri">Seri / Nomor</label>
-                                    <input type="text" name="nomorSeri" value="<?= $nomor_seri; ?>" id="nomorSeri" />
-                                  </div>
-                                </div>
-                                <div class="row">
-                                  <div class="col-lg-6">
-                                    <div>
-                                      <label for="satuan">Satuan</label>
-                                      <input type="text" name="satuan" value="<?= $satuan; ?>" id="satuan" />
-                                    </div>
-                                  </div>
-                                  <div class="col-lg-6">
-                                    <div>
-                                      <label for="nomorBukti">Nomor Bukti</label>
-                                      <input type="text" name="nomorBukti" value="<?= $nomor_bukti; ?>" id="nomorBukti" />
-                                    </div>
-                                  </div>
+                                  <label for="pihak_kedua">Pihak Kedua</label>
+                                  <input type="text" name="pihak_kedua" value="<?= $pihak_kedua ?>" id="pihak_kedua" />
                                 </div>
                                 <div>
-                                  <label for="ket">Keterangan</label>
-                                  <input type="text" name="ket" value="<?= $keterangan; ?>" id="ket" />
+                                  <label for="berkas">Keterangan</label>
+                                  <input type="file" autocomplete="off" name="berkas" value="<?= $nama_file ?>" id="berkas" />
                                 </div>
-                                <input type="hidden" name="id_masuk" value="<?= $id_masuk; ?>">
-                              </div>
-                              <div class="d-flex justify-content-center my-3">
-                                <button type="submit" class="btn btn-primary submit" name="editMasuk">Edit Data</button>
                               </div>
                             </form>
                           </div>
@@ -209,20 +133,17 @@ require 'backend/cek-login.php';
 
 
                     <!-- Hapus Data  -->
-                    <div class="modal fade" id="hapusData<?= $id_masuk; ?>">
+                    <div class="modal fade" id="hapusData<?= $id_berita; ?>">
                       <div class="modal-dialog modal-dialog-centered modal-md">
                         <div class="modal-content p-3">
                           <div class="modal-header py-2">
-                            <h4 class="modal-title fw-bold">Hapus Data Masuk</h4>
+                            <h4 class="modal-title fw-bold">Hapus Berita Acara</h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
                           <div class="modal-body">
                             <form action="" method="post">
-                              <h6>Apakah anda yakin ingin menghapus data <?= $model ?> ini ?</h6>
-                              <input type="hidden" name="id_stok" value="">
-                              <input type="hidden" name="banyak_masuk" value="<?= $banyak_masuk; ?>">
-                              <input type="hidden" name="model" value="<?= $model; ?>">
-                              <input type="hidden" name="id_masuk" value="<?= $id_masuk; ?>">
+                              <h6>Apakah anda yakin ingin menghapus Berita Acara ini ?</h6>
+                              <input type="hidden" name="id_berita" value="">
                               <div class="d-flex justify-content-center my-3">
                                 <button type="submit" class="btn btn-danger submit" name="hapusDataMasuk">Hapus</button>
                               </div>
@@ -251,7 +172,7 @@ require 'backend/cek-login.php';
     </div>
     <!-- Modal Form -->
     <div class="modal fade" id="uploadFile">
-      <div class="modal-dialog modal-dialog-centered modal-lg">
+      <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content p-3">
           <div class="modal-header py-2">
             <h4 class="modal-title fw-bold">Tambah Berita Acara Masuk</h4>
